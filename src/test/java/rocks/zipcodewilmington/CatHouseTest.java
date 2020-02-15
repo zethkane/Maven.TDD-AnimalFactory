c32baac77516a7668ed3553c2c55dd384117e4ea
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-14 19:06:21 -0500

    finished

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,12 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +16,56 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    @Test
+    public void addTest(){
+        Cat tiger = new Cat(null,null,null);
+        CatHouse.add(tiger);
+        int expected = 1;
+        int actual = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void removeCat(){
+        Cat tiger = new Cat(null, null, null);
+        CatHouse.remove(tiger);
+        int expected = 0;
+        int actual = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeCatById(){
+        Cat tiger = new Cat(null, null, 0);
+        CatHouse.remove(0);
+        int expected = 0;
+        int actual = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfCatsTest(){
+        Cat tiger = new Cat(null, null, 0);
+        CatHouse.add(tiger);
+        int expected = 1;
+        int actual = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCatById(){
+        Cat tiger = new Cat(null, null, 0);
+        CatHouse.add(tiger);
+        Cat expected = CatHouse.getCatById(0);
+        Cat actual = CatHouse.getCatById(0);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,6 +1,8 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
@@ -30,5 +32,51 @@ public class DogHouseTest {
 
         // Then
         DogHouse.getNumberOfDogs();
+    }
+
+    @Test
+    public void addDog(){
+        Dog milo = new Dog(null,null,null);
+        DogHouse.add(milo);
+
+        int expected = 1;
+        int actual = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDogById(){
+        Dog milo = new Dog(null,null,0);
+        DogHouse.add(milo);
+        DogHouse.remove(0);
+
+        int expected = 0;
+        int actual = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDogByName(){
+        Dog milo = new Dog(null,null,null);
+        DogHouse.add(milo);
+        DogHouse.remove(milo);
+
+        int expected = 0;
+        int actual = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDogById(){
+        Dog milo = new Dog(null,null,0);
+        DogHouse.add(milo);
+
+        Dog expected = DogHouse.getDogById(0);
+        Dog actual = DogHouse.getDogById(0);
+
+        Assert.assertEquals(expected, actual);
     }
 }
